6acc0a3dc479db5ef295fe139e34e56da9ee86e1
NoHeal4u <miodragnikolic@yahoo.com>
2018-01-31 21:07:29 +0100

    doradjen drugi i zavrsen treci

--- single-post2.php
+++ single-post2.php
@@ -1,5 +1,4 @@
-
-<!doctype html>
+ <!doctype html>
 <html lang="en">
 <head>
 
@@ -21,6 +20,8 @@
 
 <body>
 
+
+
  <?php include('header.php'); ?>
 
 <main role="main" class="container">
@@ -28,8 +29,18 @@
     <div class="row">
 
         <div class="col-sm-8 blog-main">
-          
-         
+           <div class="blog-post">
+                <h2 class="blog-post-title">Another blog post</h2>
+                <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+
+                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                <blockquote>
+                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                </blockquote>
+                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            </div><!-- /.blog-post -->
+
 
             <nav class="blog-pagination">
                 <a class="btn btn-outline-primary" href="#">Older</a>
@@ -41,7 +52,6 @@
         <?php include('sidebar.php'); ?>
 
        
-
     </div><!-- /.row -->
 
 </main><!-- /.container -->
@@ -51,3 +61,7 @@
 <?php include('footer.php'); ?>
 </body>
 </html>
+
+
+
+
--- single-post3.php
+++ single-post3.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -21,6 +20,8 @@
 
 <body>
 
+
+
  <?php include('header.php'); ?>
 
 <main role="main" class="container">
@@ -28,8 +29,21 @@
     <div class="row">
 
         <div class="col-sm-8 blog-main">
-          
-         
+
+          <div class="blog-post">
+                <h2 class="blog-post-title">New feature</h2>
+                <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
+
+                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <ul>
+                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+                    <li>Donec id elit non mi porta gravida at eget metus.</li>
+                    <li>Nulla vitae elit libero, a pharetra augue.</li>
+                </ul>
+                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+            </div><!-- /.blog-post -->
+
 
             <nav class="blog-pagination">
                 <a class="btn btn-outline-primary" href="#">Older</a>
@@ -41,7 +55,6 @@
         <?php include('sidebar.php'); ?>
 
        
-
     </div><!-- /.row -->
 
 </main><!-- /.container -->
@@ -51,3 +64,7 @@
 <?php include('footer.php'); ?>
 </body>
 </html>
+
+
+
+
